40688cb5e8cb4b95f2269f339410bb8fde6cd8d8
ibrahim bayburtlu <ibrahimbayburtlu5gmail.com>
2023-10-09 11:42:07 +0300

    MVC Controller Testing

--- Spring-Boot-Testing-3.0/src/main/java/com/luv2code/springmvc/controller/GradeBookController.java
+++ Spring-Boot-Testing-3.0/src/main/java/com/luv2code/springmvc/controller/GradeBookController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springmvc.controller;
 
+import com.luv2code.springmvc.models.CollegeStudent;
 import com.luv2code.springmvc.models.Gradebook;
+import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,9 +14,14 @@ public class GradeBookController {
 	@Autowired
 	private Gradebook gradebook;
 
+	@Autowired
+	private StudentAndGradeService studentAndGradeService;
+
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getStudents(Model m) {
+		Iterable<CollegeStudent> collegeStudents = studentAndGradeService.getGradeBook();
+		m.addAttribute("students",collegeStudents);
 		return "index";
 	}
 
--- Spring-Boot-Testing-3.0/target/classes/insertData.sql
+++ Spring-Boot-Testing-3.0/target/classes/insertData.sql
@@ -1,4 +1,4 @@
 insert into student(id,firstname,lastname,email_address) values (2,'Eric','Roby','eric.roby@luv2code_school.com')
-    insert into student(id,firstname,lastname,email_address) values (3,'Eric','Roby','eric.roby@luv2code_school.com')
+insert into student(id,firstname,lastname,email_address) values (3,'Eric','Roby','eric.roby@luv2code_school.com')
 insert into student(id,firstname,lastname,email_address) values (4,'Eric','Roby','eric.roby@luv2code_school.com')
 insert into student(id,firstname,lastname,email_address) values (5,'Eric','Roby','eric.roby@luv2code_school.com')
